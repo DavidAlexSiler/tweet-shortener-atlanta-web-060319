122e5d303937cf3b90dcfcf693e23a5544ab829c
Alex Siler <davidalexsiler@gmail.com>
2019-06-03 01:50:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,10 +38,10 @@ def word_substituter(string)
   end.join(" ")
 end  
 
-# bulk_tweet_shortner(array)
-#   array.each do |tweet| 
-#     puts word_substituter(tweet)
-# end
+bulk_tweet_shortner(array)
+  array.each do |tweet| 
+    puts word_substituter(tweet)
+end
 
 def selective_tweet_shortener(string)
   if string.length > 140
